a2ec5567266383f7f8ebe73808c92475c5bbc45b
R8nDr0p <rainchrstn@gmail.com>
2023-06-28 15:35:54 +0800

    installed dependencies and imported bootstrap and react-query on main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-
-
 function App() {
-
-
   return (
     <>
-    
-        
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1>Hello Group 4</h1>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
