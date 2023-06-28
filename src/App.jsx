b3718d351ccdfca11b7381bcd6d72f8073673710
R8nDr0p <rainchrstn@gmail.com>
2023-06-28 16:16:37 +0800

    added layout and setup app.jsx for the react router dom .. started creating the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 function App() {
   return (
     <>
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <h1>Hello Group 4</h1>
-          </div>
-        </div>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
